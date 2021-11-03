31da735ead68aa214c32acba77dd111d44c9db2f
GvMazzon25 <gvmazzon25@gmail.com>
2021-11-03 14:53:19 +0100

    face-end

--- script.js
+++ script.js
@@ -6,3 +6,63 @@ con difficoltà 3 => tra 1 e 49
 Quando l’utente clicca su ogni cella, la cella cliccata si colora di azzurro.
 
  */
+
+
+//Refs 
+
+const playBtn = document.getElementById('play');
+const levels = document.getElementById('levels');
+const wrapGrid = document.querySelector('.wrap-grid');
+
+//start
+
+playBtn.addEventListener('click', () =>{
+    wrapGrid.innerHTML = '';
+
+    let cellsNumber;
+    let cellsPerSide;
+
+    switch (levels.value){
+        case '1':
+            cellsNumber = 100;
+            cellsPerSide = 10;
+            break
+            case '2':
+                cellsNumber = 81;
+                cellsPerSide = 9;
+                break
+            case '3':
+                cellsNumber = 49;
+                cellsPerSide = 7;
+            break
+    }
+
+    const grid = document.createElement('div');
+    grid.classList.add('grid');
+
+    for (let i = 1; i <= cellsNumber; i++){
+        const square = createGridSquare(i, cellsPerSide)
+
+        square.addEventListener ('click',() => {
+            square.classList.add('clicked');
+        })
+
+        grid.append(square)
+
+    }
+
+    wrapGrid.append(grid);
+})
+
+function createGridSquare(num, cells){
+    const nodo = document.createElement('div');
+
+    nodo.classList.add('square');
+
+    nodo.style.width = `calc(100% / ${cells})`;
+    nodo.style.height = `calc(100% / ${cells})`;
+
+    nodo.append(num);
+
+    return nodo;
+}
